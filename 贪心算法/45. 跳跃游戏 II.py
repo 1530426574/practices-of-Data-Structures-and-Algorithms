6e325171dbb162d4ab8d562ffd2d5d882f63db81
xuwei <wei.xu@yamu.com>
2020-06-01 09:30:02 +0800

    add 45 跳跃游戏II

--- 贪心算法/45. 跳跃游戏 II.py
+++ 贪心算法/45. 跳跃游戏 II.py
@@ -1,5 +1,32 @@
 from typing import List
 class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxPos, end, setp = 0, 0, 0
+        for i in range(n-1):
+            if  i <= maxPos:
+                maxPos = max(maxPos,i+nums[i])
+                if i == end:                   #在可选范围内，都会走一步的，并确定下一个可选范围
+                    end = maxPos
+                    setp += 1
+        return setp
+
+
+from typing import List
+class Solution11:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        maxPos, end, setp, res, imax= 0, 0, 0, [], 0
+        for i in range(n-1):
+            if  i <= maxPos:
+                maxPos = max(maxPos,i+nums[i])
+                if i == end:
+                    end = maxPos
+                    setp += 1
+        return setp
+
+
+class Solution1:
     def jump(self, nums: List[int]) -> List[int]:
         length, rightmost,res= len(nums), 0,[]
         for i in range(length-1):
@@ -16,7 +43,7 @@ print(s.jump([2,1,1,0,4]))
 
 #例如，对于数组 [2,3,1,2,4,2,3]
 
-class Solution(object):
+class Solution2(object):
     def jump(self, nums):
         """
         :type nums: List[int]
@@ -29,7 +56,7 @@ class Solution(object):
         return step
 
 
-class Solution:
+class Solution3:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
         maxPos, end, step = 0, 0, 0
--- 贪心算法/link.py
+++ 贪心算法/link.py
@@ -7,6 +7,6 @@ https://leetcode-cn.com/problems/lemonade-change/description/
 https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/description/
 https://leetcode-cn.com/problems/assign-cookies/description/
 https://leetcode-cn.com/problems/walking-robot-simulation/description/
-https://leetcode-cn.com/problems/jump-game/
- https://leetcode-cn.com/problems/jump-game-ii/
+https://leetcode-cn.com/problems/jump-game/ 、
+https://leetcode-cn.com/problems/jump-game-ii/
 """
