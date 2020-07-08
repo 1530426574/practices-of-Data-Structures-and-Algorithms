31144579e18edee8eecb9706a6f57a23d11a488f
xuwei <wei.xu@yamu.com>
2020-07-08 09:35:53 +0800

    add Insertion Sort.py

---
+++ 排序算法/Insertion Sort.py
@@ -0,0 +1,14 @@
+def insert_sort(arry):
+    for i in range(1,len(arry)):
+        card = arry[i]                     #current card
+        j = i - 1                          #prev card
+        while j >= 0  and card < arry[j]:  #current < prev
+            arry[j+1] = arry[j]            #shift right
+            j -= 1                         #continue compare
+        arry[j+1] =  card                  #find the correct position
+    return arry
+
+
+
+arry = [12, 11, 13, 5, 6]
+print(insert_sort(arry))
