28e654b45728443ba790f7c32dd5ad6195328255
xuwei <wei.xu@yamu.com>
2020-07-27 09:39:42 +0800

    add 14

---
+++ 字符串算法/14. 最长公共前缀.py
@@ -0,0 +1,66 @@
+"""
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+示例 1:
+
+输入: ["flower","flow","flight"]
+输出: "fl"
+示例 2:
+
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-common-prefix
+
+"""
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        res = ""
+        for tmp in zip(*strs):
+            tmp_set = set(tmp)
+            if len(tmp_set) == 1:
+                res += tmp[0]
+            else:
+                break
+        return res
+
+class Solution2:
+    def longestCommonPrefix(self, s: List[str]) -> str:
+        if not s:
+            return ""
+        res = s[0]
+        i = 1
+        while i < len(s):
+            while s[i].find(res) != 0:
+                res = res[0:len(res)-1]
+            i += 1
+        return res
+
+class Solution3:
+    def longestCommonPrefix(self, s: List[str]) -> str:
+        if not s:
+            return ""
+        s.sort()
+        n = len(s)
+        a = s[0]
+        b = s[n-1]
+        res = ""
+        for i in range(len(a)):
+            if i < len(b) and a[i] == b[i]:
+                res += a[i]
+            else:
+                break
+        return res
+
+# 作者：powcai
+# 链接：https://leetcode-cn.com/problems/longest-common-prefix/solution/duo-chong-si-lu-qiu-jie-by-powcai-2/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
